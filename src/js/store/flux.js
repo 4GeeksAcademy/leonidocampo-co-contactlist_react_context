42cbd17faac1bf1837421e60ac24c895d1900394
Leonid ocampo <115747563+leonidocampo-co@users.noreply.github.com>
2023-05-31 17:08:23 +0000

    cambios

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -12,18 +12,111 @@ const getState = ({ getStore, getActions, setStore }) => {
 					background: "white",
 					initial: "white"
 				}
-			]
+			],
+
+			toDoList: []
 		},
 		actions: {
 			// Use getActions to call a function within a fuction
 			exampleFunction: () => {
 				getActions().changeColor(0, "green");
 			},
+
 			loadSomeData: () => {
 				/**
 					fetch().then().then(data => setStore({ "foo": data.bar }))
 				*/
 			},
+
+			createToDo: async () => {
+				await fetch('https://assets.breatheco.de/apis/fake/todos/user/leonidocampo-co', {
+
+					method: "POST",
+
+					body: JSON.stringify([]),
+
+					headers: {
+
+						"Content-Type": "application/json"
+
+					}
+
+				})
+
+					.then(resp => {
+
+						console.log(resp.ok); // will be true if the response is successfull
+
+						console.log(resp.status); // the status code = 200 or code = 400 etc.
+
+						console.log(resp.text()); // will try return the exact result as string
+
+						return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
+
+					})
+
+					.then(data => {
+
+						//here is where your code should start after the fetch finishes
+
+						console.log(data); //this will print on the console the exact object received from the server
+
+					})
+
+					.catch(error => {
+
+						//error handling
+
+						console.log(error);
+
+					});
+			},
+
+
+			loadToDo: () => {
+				fetch('https://assets.breatheco.de/apis/fake/todos/user/leonidocampo-co', {
+
+					method: "GET",
+					
+					headers: {
+
+						"Content-Type": "application/json"
+
+					}
+
+				})
+
+					.then(resp => {
+
+						console.log(resp.ok); // will be true if the response is successfull
+
+						console.log(resp.status); // the status code = 200 or code = 400 etc.
+
+						console.log(resp.text()); // will try return the exact result as string
+
+						return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
+
+					})
+
+					.then(data => {
+
+						//here is where your code should start after the fetch finishes
+
+						setStore({toDoList:data})
+
+					})
+
+					.catch(error => {
+
+						//error handling
+
+						console.log(error);
+
+					});
+
+			},
+
+
 			changeColor: (index, color) => {
 				//get the store
 				const store = getStore();
@@ -34,6 +127,8 @@ const getState = ({ getStore, getActions, setStore }) => {
 					if (i === index) elm.background = color;
 					return elm;
 				});
+
+
 
 				//reset the global store
 				setStore({ demo: demo });

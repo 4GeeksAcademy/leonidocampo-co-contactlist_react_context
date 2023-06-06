a4a76b5e342e09bb0bd04f033982a71ef2d6b4d0
Leonid ocampo <115747563+leonidocampo-co@users.noreply.github.com>
2023-06-06 23:27:59 +0000

    cambios en lista

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,32 @@
 import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
+import { ContactCard } from "../component/ContactCard";
 import "../../styles/home.css";
+import rigoImage from "../../img/rigo-baby.jpg";
 
 export const Home = () => (
-	<div className="text-center mt-5">
+	<div className="d-flex flex-column justify-center mt-5">
 		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
+		<div className="list-group contact-list">
+			<ContactCard
+				name={"Leo"}
+				address={"Medellin"}
+				email={"leo@hotmail.com"}
+				phone={"3761589"}
+				img={rigoImage}
+			/>
+			<ContactCard
+				name={"Leo"}
+				address={"Medellin"}
+				email={"leo@hotmail.com"}
+				phone={"3761589"}
+				img={rigoImage} />
+			<ContactCard
+				name={"Leo"}
+				address={"Medellin"}
+				email={"leo@hotmail.com"}
+				phone={"3761589"}
+				img={rigoImage} />
+		</div>
+
 	</div>
 );
